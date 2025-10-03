28727df67090393785b9350fc5a9faeed0ac6daa
Flame787 <mbrezovic77@gmail.com>
2025-10-03 20:51:29 +0200

    added todos

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
+import Todo from "../models/todo";
+
 // function Todos(props: { items: string[], children }) {
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   // FC - Function Component (Generic type definition built into React package)
   // React.FC<{items: string[]}> - defining the type of props that this component expects to receive
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
