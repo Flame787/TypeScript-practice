0af728ac8d22a62675cfb82c8990ad9e9aeb3402
Flame787 <mbrezovic77@gmail.com>
2025-10-05 01:28:29 +0200

    added form & css files

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,11 +1,25 @@
-const NewTodo = () => {
+import { useRef } from "react";
+import classes from "./NewTodo.module.css";
+
+const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+    props.onAddTodo(enteredText);
   };
+
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button type="submit">Add Todo</button>
     </form>
   );
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 import Todo from "../models/todo";
+import classes from "./Todos.module.css";
 
 // function Todos(props: { items: string[], children }) {
 
@@ -8,7 +9,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
   // FC - Function Component (Generic type definition built into React package)
   // React.FC<{items: string[]}> - defining the type of props that this component expects to receive
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem key={item.id} text={item.text} />
       ))}
