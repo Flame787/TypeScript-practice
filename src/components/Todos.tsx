cd31b6de0a7afb656b646f0d6914ac225b0350dc
Flame787 <mbrezovic77@gmail.com>
2025-10-04 23:39:37 +0200

    functional todos

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import TodoItem from "./TodoItem";
 import Todo from "../models/todo";
 
 // function Todos(props: { items: string[], children }) {
@@ -10,7 +10,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
